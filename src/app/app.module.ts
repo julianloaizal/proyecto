8f31ae2c5694f20b592aa20fb882ea2ae0ded279
Julian Loaiza <julian.loaizal@udea.edu.co>
2018-10-02 14:27:03 -0500

    creación de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule
