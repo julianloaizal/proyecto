d2b23ae8110a19d0f0044b6f885c94b50d749ed3
Julian Loaiza <julian.loaizal@udea.edu.co>
2018-10-03 08:40:33 -0500

    Modificación en la vista proyecto

---
+++ src/app/components/proyectos/proyect.ts
@@ -0,0 +1,18 @@
+
+export class Proyect {
+name: String;
+type: String;
+modality: String;
+responsable: String;
+  adviser: String;
+participants: String[];
+  creationdate: String;
+  initialdate: String;
+finaldate: String;
+qualification: Number;
+state: String;
+deliverables: Number;
+generalpurpose: String;
+  specificobjectives: String;
+recognitions: String;
+}
--- src/app/components/proyectos/proyectos.component.ts
+++ src/app/components/proyectos/proyectos.component.ts
@@ -1,14 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Proyect } from './proyect';
 
 @Component({
   selector: 'app-proyectos',
   templateUrl: './proyectos.component.html'
 })
 export class ProyectosComponent implements OnInit {
-
+  proyect: Proyect = {
+    name: 'Proyecto de medio ambiente',
+      type: 'Proyecto de grado',
+      modality: 'Pregrado',
+      responsable: 'Alvaro Nuñez',
+      adviser: 'Rodolfo Andres',
+      participants: ['Andres Jeronimo'],
+      creationdate: '2018-09-23',
+      initialdate: '2018-09-25',
+      finaldate: '2018-10-23',
+      qualification: 3.5,
+      state: 'Pendiente',
+      deliverables: 1,
+      generalpurpose: 'Terminar el trabajo de grado para poder trabajar',
+      specificobjectives: 'Muchos, luego los vemos',
+      recognitions: 'Ninguna'
+    };
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
